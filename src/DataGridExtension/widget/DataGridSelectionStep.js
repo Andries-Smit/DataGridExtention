886c4ef0c58580742025ba3f72f4dc393659601a
Andries Smit <mr.andries.smit@gmail.com>
2014-12-22 09:44:46 +0100

    Fix Step Selection Mx 5.11

--- src/DataGridExtension/widget/DataGridSelectionStep.js
+++ src/DataGridExtension/widget/DataGridSelectionStep.js
@@ -1,3 +1,24 @@
+/*global mx, mxui, mendix, dojo, require, console, define, module, logger, window, setTimeout */
+/**
+    Data Grid Extension Step Selection
+    ========================
+    @file      : DataGridSelectionStep.js
+    @version   : 1.1
+    @author    : Andries Smit 
+    @date      : 20-12-2014
+    @copyright : Flock of Birds International BV
+
+    Change log
+    ========================
+    ISSUES:
+    
+    TODO:
+    
+    DONE:
+    Fix datasource selection in case a page is loaded mulitple times (result in cashed objects)
+    Mendix 5.11 selection attribute changed, cause wrong/ no selection on refresh.
+
+*/
 require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
     "use strict";
     var widget = {
@@ -18,13 +39,6 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
         context: null,
         dataView: null,
         button: null,
-
-        // ISSUES:
-        // 
-        // TODO:
-        // 
-        // DONE:
-        // Fix datasource selection in case a page is loaded mulitple times (result in cashed objects)
 
         postCreate: function () {
             try {
@@ -146,9 +160,9 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
                     selectedIndex--; // previous row index
                     var nextRow = rows[selectedIndex];
                     var guid = this.grid.domData(nextRow, "mendixguid");
-                    this.grid._selectedGuids = [guid];
+                    this.setSelectedGuid(guid);
                     this.grid.selectRow(nextRow);
-                    this.grid.shareSelected();
+                    this.shareSelected();
                     this.checkEnableButtons();
                 } else {
                     console.log("begining of page");
@@ -159,9 +173,9 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
                                 this.grid.deselectAll();
                                 var lastRow = rows[rows.length - 1];
                                 var guid = this.grid.domData(lastRow, "mendixguid");
-                                this.grid._selectedGuids = [guid];
+                                this.setSelectedGuid(guid);
                                 this.grid.selectRow(lastRow);
-                                this.grid.shareSelected(); // notify others including listinging Data View
+                                this.shareSelected();
                                 this.checkEnableButtons();
                             }));
                         }
@@ -169,7 +183,21 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
                 }
             }
         },
+        
+        setSelectedGuid: function(guid){
+            if(this.grid.hasOwnProperty("_selectedGuids")){
+                this.grid._selectedGuids = [guid]; // before mx 5.11 
+            } else {
+                this.grid.selection = [guid];
+            }  
+        },
 
+        shareSelected: function(){
+            // notify others including listinging Data View
+            this.grid.shareSelected && this.grid.shareSelected(); // before Mx5.11
+            this.grid._shareSelection && this.grid._shareSelection(this.grid._metaEntity.getEntity()); // from Mx 5.11
+        },
+        
         nextRow: function () {
             // select next row
             if(! this.isLastRowSelected()){
@@ -181,9 +209,9 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
                     selectedIndex++; // next index
                     var nextRow = rows[selectedIndex];
                     var guid = this.grid.domData(nextRow, "mendixguid");
-                    this.grid._selectedGuids = [guid]; // set mx-grid property 
+                    this.setSelectedGuid(guid); // set mx-grid property 
                     this.grid.selectRow(nextRow);
-                    this.grid.shareSelected(); // notify others including listinging Data View
+                    this.shareSelected();
                     this.checkEnableButtons();
                 } else { // end of page
                     console.log("end of page");
@@ -194,9 +222,9 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
                                 this.grid.deselectAll(); // deselect in case manual changed page
                                 var firstRow = rows[0]; // select first row of new page
                                 var guid = this.grid.domData(firstRow, "mendixguid");
-                                this.grid._selectedGuids = [guid]; // set mx-grid property
+                                this.setSelectedGuid(guid); // set mx-grid property
                                 this.grid.selectRow(firstRow);
-                                this.grid.shareSelected(); // notify others including listinging Data View
+                                this.shareSelected();
                                 this.checkEnableButtons();
                             }));
                         }
@@ -237,5 +265,4 @@ require(["dojo", "dijit", "dojo/NodeList-traverse"], function (dojo, dijit) {
     };
 
     mxui.widget.declare("DataGridExtension.widget.DataGridSelectionStep", widget);
-    //mxui.widget.declare("GridExtention.widget.GridExtentionNoContext", widget);
 });
