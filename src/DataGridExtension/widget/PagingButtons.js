0ea7f761adcb9d3286f6079bddc79e2d6e118ca3
Eric Tieniber <tieniber@gmail.com>
2017-02-02 12:34:55 -0500

    fix page button hiding + gulp + test project 6.2.1

--- src/DataGridExtension/widget/PagingButtons.js
+++ src/DataGridExtension/widget/PagingButtons.js
@@ -1,12 +1,12 @@
 //----------------------------------------------------------------------
-// Section for dynamic showing paging and Empty table 
+// Section for dynamic showing paging and Empty table
 //----------------------------------------------------------------------
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase"
 ], function(declare, _WidgetBase) {
     //"use strict";
-    
+
     return declare(null, {
         inputargs: {
             hideUnusedPaging: false,
@@ -37,14 +37,16 @@ define([
                     else
                         dojo.style(this.grid.pagingBarNode, "display", "block");
                 }
-                if (this.hideUnusedPaging && this.grid.pagingBarNode.childNodes.length > 0) {
+                if (this.hideUnusedPaging && this.grid.pagingBarNode.children.length > 0) {
                     var countPages = Math.ceil(this.grid._dataSource.getSetSize() / this.grid._dataSource._pageSize);
-                    if (countPages <= this.firstLastMinPages) {
-                        dojo.style(this.grid.pagingBarNode.childNodes[0], "display", "none");
-                        dojo.style(this.grid.pagingBarNode.childNodes[8], "display", "none");
+					var lastButtonIndex = this.grid.pagingBarNode.children.length - 1;
+					if (countPages <= this.firstLastMinPages) {
+                        dojo.style(this.grid.pagingBarNode.children[0], "display", "none");
+						dojo.style(this.grid.pagingBarNode.children[lastButtonIndex], "display", "none");
+
                     } else {
-                        dojo.style(this.grid.pagingBarNode.childNodes[0], "display", "inline-block");
-                        dojo.style(this.grid.pagingBarNode.childNodes[8], "display", "inline-block");
+                        dojo.style(this.grid.pagingBarNode.children[0], "display", "inline-block");
+                        dojo.style(this.grid.pagingBarNode.children[lastButtonIndex], "display", "inline-block");
                     }
                 }
             }
